8d09a307b16352f220026c5183f2c0ee047d3c28
kbtania1@gmail.com <kbtania1@gmail.com>
2020-03-12 19:05:24 +0200

    1

---
+++ Lab10/Task1/Number.cs
@@ -0,0 +1,49 @@
+﻿using System;
+
+namespace Lab10Task1
+{
+    class Number
+    {
+        private int num;
+
+        public int Num
+        {
+            get { return num; }
+            set
+            {
+                if (value < 0)
+                    throw new Exception("Number must be positive integer.");
+                num = value;
+            }
+        }
+
+        public Number(int userNum)
+        {
+            Num = userNum;
+        }
+
+        public override string ToString()
+        {
+            return $"{num}";
+        }
+
+        public int CountOfNums()
+        {
+            return num.ToString().Length;
+        }
+
+        public int SumOfDigits()
+        {
+            int sum = 0;
+            while (num != 0)
+            {
+                sum += num % 10;
+                num /= 10;
+            }
+
+            return sum;
+        }
+    }
+
+}
+
---
+++ Lab10/Task1/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+
+namespace Lab10Task1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            try
+            {
+                Console.Write("Enter number: ");
+                int number = Convert.ToInt32(Console.ReadLine());
+                Number n = new Number(number);
+
+                Console.WriteLine($"The number: {n.ToString()}");
+                Console.WriteLine($"Count of digits: {n.CountOfNums()}");
+                Console.WriteLine($"Sum of digits: {n.SumOfDigits()}");
+            }
+            catch
+            {
+                Console.WriteLine("Number must be positive integer.");
+            }
+        }
+    }
+}
