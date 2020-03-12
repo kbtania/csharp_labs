4b0d0e52d23a626145a7b5013b37ba0f0e3bff17
kbtania <57090076+kbtania@users.noreply.github.com>
2020-03-12 22:57:32 +0200

    Update Program.cs

--- Lab10/Task2/Program.cs
+++ Lab10/Task2/Program.cs
@@ -10,15 +10,13 @@ namespace Lab10Task2
             DateTime dateProductionSeller = new DateTime(2001, 1, 1);
             DateTime dateProductionCar= new DateTime(2016, 1, 1);
             DateTime currDate = new DateTime(2020, 1, 1);
-            Manufacturer m = new Manufacturer("Hyundai Motor Group", dateProductionManufacturer,
-                03144653, 100000);
+            Manufacturer m = new Manufacturer("Hyundai Motor Group", dateProductionManufacturer, 03144653, 100000);
             Seller s = new Seller("SellingCars", dateProductionSeller, 123, 95 );
             Owner o = new Owner("Pet", 111);
             
             Console.WriteLine("------- Car info -------");
 
-            Automobile auto = new Automobile("Hyundai", "black", 
-                3232, m, s, o, dateProductionCar);
+            Automobile auto = new Automobile("Hyundai", "black", 3232, m, s, o, dateProductionCar);
             Console.WriteLine(auto);
             Console.WriteLine($"The age of car is {auto.GetAge(currDate)}");
         
