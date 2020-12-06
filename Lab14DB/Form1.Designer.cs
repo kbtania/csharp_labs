b1651d9a0061b25ca3a12bc41f16c51ed3c15881
kbtania1@gmail.com <kbtania1@gmail.com>
2020-12-06 15:52:59 +0200

    Added Lab14

---
+++ Lab14DB/Form1.Designer.cs
@@ -0,0 +1,229 @@
+﻿namespace Lab14DB
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Обязательная переменная конструктора.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Освободить все используемые ресурсы.
+        /// </summary>
+        /// <param name="disposing">истинно, если управляемый ресурс должен быть удален; иначе ложно.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Код, автоматически созданный конструктором форм Windows
+
+        /// <summary>
+        /// Требуемый метод для поддержки конструктора — не изменяйте 
+        /// содержимое этого метода с помощью редактора кода.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.button1 = new System.Windows.Forms.Button();
+            this.dataGridView1 = new System.Windows.Forms.DataGridView();
+            this.button2 = new System.Windows.Forms.Button();
+            this.label1 = new System.Windows.Forms.Label();
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.button3 = new System.Windows.Forms.Button();
+            this.label2 = new System.Windows.Forms.Label();
+            this.button4 = new System.Windows.Forms.Button();
+            this.label3 = new System.Windows.Forms.Label();
+            this.button5 = new System.Windows.Forms.Button();
+            this.label4 = new System.Windows.Forms.Label();
+            this.button6 = new System.Windows.Forms.Button();
+            this.textBox2 = new System.Windows.Forms.TextBox();
+            this.button7 = new System.Windows.Forms.Button();
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(982, 586);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(145, 52);
+            this.button1.TabIndex = 0;
+            this.button1.Text = "Відобразити";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // dataGridView1
+            // 
+            this.dataGridView1.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.dataGridView1.Location = new System.Drawing.Point(12, 41);
+            this.dataGridView1.Name = "dataGridView1";
+            this.dataGridView1.RowHeadersWidth = 62;
+            this.dataGridView1.RowTemplate.Height = 28;
+            this.dataGridView1.Size = new System.Drawing.Size(698, 269);
+            this.dataGridView1.TabIndex = 1;
+            // 
+            // button2
+            // 
+            this.button2.Location = new System.Drawing.Point(982, 514);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(145, 51);
+            this.button2.TabIndex = 2;
+            this.button2.Text = "Оновити";
+            this.button2.UseVisualStyleBackColor = true;
+            this.button2.Click += new System.EventHandler(this.button2_Click);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(734, 51);
+            this.label1.MaximumSize = new System.Drawing.Size(200, 0);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(199, 40);
+            this.label1.TabIndex = 3;
+            this.label1.Text = "Намолодшиий власник в місті";
+            // 
+            // textBox1
+            // 
+            this.textBox1.Location = new System.Drawing.Point(939, 51);
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(124, 26);
+            this.textBox1.TabIndex = 4;
+            // 
+            // button3
+            // 
+            this.button3.Location = new System.Drawing.Point(1077, 44);
+            this.button3.Name = "button3";
+            this.button3.Size = new System.Drawing.Size(126, 40);
+            this.button3.TabIndex = 5;
+            this.button3.Text = "Показати";
+            this.button3.UseVisualStyleBackColor = true;
+            this.button3.Click += new System.EventHandler(this.button3_Click);
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(734, 108);
+            this.label2.MaximumSize = new System.Drawing.Size(200, 0);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(194, 40);
+            this.label2.TabIndex = 6;
+            this.label2.Text = "Випадки реєстрування неповнолітніми особами";
+            // 
+            // button4
+            // 
+            this.button4.Location = new System.Drawing.Point(939, 117);
+            this.button4.Name = "button4";
+            this.button4.Size = new System.Drawing.Size(126, 40);
+            this.button4.TabIndex = 7;
+            this.button4.Text = "Показати";
+            this.button4.UseVisualStyleBackColor = true;
+            this.button4.Click += new System.EventHandler(this.button4_Click);
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(734, 169);
+            this.label3.MaximumSize = new System.Drawing.Size(200, 0);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(198, 100);
+            this.label3.TabIndex = 8;
+            this.label3.Text = "Випадки, коли більше 50% статутного капіталу внесено підприємцем, що проживає в і" +
+    "ншому  місті";
+            // 
+            // button5
+            // 
+            this.button5.Location = new System.Drawing.Point(939, 195);
+            this.button5.Name = "button5";
+            this.button5.Size = new System.Drawing.Size(126, 40);
+            this.button5.TabIndex = 9;
+            this.button5.Text = "Показати";
+            this.button5.UseVisualStyleBackColor = true;
+            this.button5.Click += new System.EventHandler(this.button5_Click);
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Location = new System.Drawing.Point(734, 304);
+            this.label4.MaximumSize = new System.Drawing.Size(200, 0);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(200, 80);
+            this.label4.TabIndex = 10;
+            this.label4.Text = "Список магазинів, якими володіє особа в порядку спадання вкладеного в них капітал" +
+    "у";
+            // 
+            // button6
+            // 
+            this.button6.Location = new System.Drawing.Point(1077, 329);
+            this.button6.Name = "button6";
+            this.button6.Size = new System.Drawing.Size(126, 40);
+            this.button6.TabIndex = 11;
+            this.button6.Text = "Показати";
+            this.button6.UseVisualStyleBackColor = true;
+            this.button6.Click += new System.EventHandler(this.button6_Click);
+            // 
+            // textBox2
+            // 
+            this.textBox2.Location = new System.Drawing.Point(939, 329);
+            this.textBox2.Name = "textBox2";
+            this.textBox2.Size = new System.Drawing.Size(124, 26);
+            this.textBox2.TabIndex = 12;
+            // 
+            // button7
+            // 
+            this.button7.Location = new System.Drawing.Point(634, 585);
+            this.button7.Name = "button7";
+            this.button7.Size = new System.Drawing.Size(208, 53);
+            this.button7.TabIndex = 13;
+            this.button7.Text = "button7";
+            this.button7.UseVisualStyleBackColor = true;
+            this.button7.Click += new System.EventHandler(this.button7_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(9F, 20F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(1501, 668);
+            this.Controls.Add(this.button7);
+            this.Controls.Add(this.textBox2);
+            this.Controls.Add(this.button6);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.button5);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.button4);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.button3);
+            this.Controls.Add(this.textBox1);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.button2);
+            this.Controls.Add(this.dataGridView1);
+            this.Controls.Add(this.button1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.DataGridView dataGridView1;
+        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.Button button3;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Button button4;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Button button5;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.Button button6;
+        private System.Windows.Forms.TextBox textBox2;
+        private System.Windows.Forms.Button button7;
+    }
+}
+
---
+++ Lab14DB/Form1.cs
@@ -0,0 +1,152 @@
+﻿using System;
+using System.Data;
+using System.Data.SqlClient;
+using System.Windows.Forms;
+
+namespace Lab14DB
+{
+    public partial class Form1 : Form
+    {
+        string CONNECTION_STRING = @"Data Source=(LocalDB)\MSSQLLocalDB;AttachDbFilename=C:\Lab14DB\Shops.mdf;Integrated Security = True";
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            SqlConnection connection = new SqlConnection(CONNECTION_STRING);
+            connection.Open();
+            SqlDataAdapter adapter = new SqlDataAdapter("select * from Shop", CONNECTION_STRING);
+            DataTable table = new DataTable();
+            adapter.Fill(table);
+            dataGridView1.DataSource = table;
+            connection.Close();
+            MessageBox.Show(CONNECTION_STRING);
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            for (int i = 0; i < dataGridView1.RowCount; i++)
+            {
+
+                SqlConnection connection = new SqlConnection(CONNECTION_STRING);
+                connection.Open();
+                SqlCommand command = new SqlCommand("Update Shop Set Name=@Name,Profile=@Profile,City=@City," +
+                    "Street=@Street,HouseNum=@HouseNum,Capital=@Capital,Phone=@Phone where Id=@Id", connection);
+                command.Parameters.AddWithValue("@Id", Convert.ToInt32(dataGridView1[0, i].Value));
+                command.Parameters.AddWithValue("@Name", Convert.ToString(dataGridView1[1, i].Value));
+                command.Parameters.AddWithValue("@Profile", Convert.ToString(dataGridView1[2, i].Value));
+                command.Parameters.AddWithValue("@City", Convert.ToString(dataGridView1[3, i].Value));
+                command.Parameters.AddWithValue("@Street", Convert.ToString(dataGridView1[4, i].Value));
+                command.Parameters.AddWithValue("@HouseNum", Convert.ToInt32(dataGridView1[5, 1].Value));
+                command.Parameters.AddWithValue("@Capital", Convert.ToDecimal(dataGridView1[6, i].Value));
+                command.Parameters.AddWithValue("@Phone", Convert.ToInt32(dataGridView1[7, i].Value));
+                command.ExecuteNonQuery();
+                connection.Close();
+            }
+        }
+
+        // наймолодший підприємець, що має власність у вказаному місті 
+        private void button3_Click(object sender, EventArgs e)
+        {
+            string city = textBox1.Text;
+            SqlConnection connection = new SqlConnection(CONNECTION_STRING);
+            connection.Open();
+            string query = "SELECT TOP 1[Owner].[Name] AS Власник, [Owner].BirthDate, Shop.[Name], Shop.City " +
+                "FROM[Owner] " +
+                "JOIN OwnerShop ON[Owner].Id = OwnerShop.OwnerId " +
+                "JOIN Shop ON OwnerShop.ShopId = Shop.Id " +
+                "WHERE Shop.City = @city " +
+                "ORDER BY [Owner].BirthDate DESC" ;
+
+            SqlDataAdapter da = new SqlDataAdapter();
+            SqlCommand cmd = new SqlCommand(query, connection);
+            da.SelectCommand = cmd;
+            cmd.Parameters.Add("@city", SqlDbType.NVarChar, 30).Value = city;
+
+            DataTable table = new DataTable();
+            da.Fill(table);
+            dataGridView1.DataSource = table;
+            connection.Close();
+        }
+
+        // випадки, коли реєструвалося володіння особами, які не доягли 18 років
+        private void button4_Click(object sender, EventArgs e)
+        {
+            SqlConnection connection = new SqlConnection(CONNECTION_STRING);
+            connection.Open();
+            string query = "SELECT [Owner].[Name], [Owner].BirthDate, [Owner].RegistrationDate " +
+                "FROM[Owner] " +
+                "WHERE DATEDIFF(YEAR, [Owner].BirthDate, [Owner].RegistrationDate) <= 18";
+
+            SqlDataAdapter da = new SqlDataAdapter();
+            SqlCommand cmd = new SqlCommand(query, connection);
+            da.SelectCommand = cmd;
+            DataTable table = new DataTable();
+            da.Fill(table);
+            dataGridView1.DataSource = table;
+            connection.Close();
+        }
+
+        // випадки, коли більше 50% статутного капіталу магазину внесено підприємцем,
+        // який проживає в іншому місті
+        private void button5_Click(object sender, EventArgs e)
+        {
+            SqlConnection connection = new SqlConnection(CONNECTION_STRING);
+            connection.Open();
+            string query = "SELECT [Owner].[Name], [Owner].City, Shop.[Name], Shop.City, OwnerShop.Investment " +
+                "FROM[Owner] JOIN OwnerShop ON[Owner].Id = OwnerShop.OwnerId " +
+                "JOIN Shop ON OwnerShop.ShopId = Shop.Id " +
+                "Where[Owner].City != Shop.City AND(Shop.Capital / 2) < OwnerShop.Investment";
+
+            SqlDataAdapter da = new SqlDataAdapter();
+            SqlCommand cmd = new SqlCommand(query, connection);
+            da.SelectCommand = cmd;
+            DataTable table = new DataTable();
+            da.Fill(table);
+            dataGridView1.DataSource = table;
+            connection.Close();
+        }
+
+        // список магазинів, якими володіє Особа в порядку спадання 
+        // вкладеного в них капіталу
+        private void button6_Click(object sender, EventArgs e)
+        {
+            string person = textBox2.Text;
+            SqlConnection connection = new SqlConnection(CONNECTION_STRING);
+            connection.Open();
+            string query = "SELECT [Owner].[Name], Shop.[Name], Shop.Profile, OwnerShop.Investment " +
+                "FROM[Owner] JOIN OwnerShop ON[Owner].Id = OwnerShop.OwnerId " +
+                "JOIN Shop ON OwnerShop.ShopId = Shop.Id " +
+                "WHERE[Owner].[Name] LIKE @person " +
+                "ORDER BY [OwnerShop].Investment DESC";
+
+            SqlDataAdapter da = new SqlDataAdapter();
+            SqlCommand cmd = new SqlCommand(query, connection);
+            da.SelectCommand = cmd;
+            cmd.Parameters.Add("@person", SqlDbType.NVarChar, 100).Value = $"%{person}%";
+
+            DataTable table = new DataTable();
+            da.Fill(table);
+            dataGridView1.DataSource = table;
+            connection.Close();
+        }
+
+        private void button7_Click(object sender, EventArgs e)
+        {
+            SqlConnection connection = new SqlConnection(CONNECTION_STRING);
+            connection.Open();
+            SqlDataAdapter adapter = new SqlDataAdapter(
+                "SELECT S.[Name], S.City, O.[Name] " +
+                "FROM OwnerShop AS OS " +
+                "INNER JOIN Shop AS S " +
+                "ON OS.ShopId = S.Id LEFT JOIN[Owner] AS O " +
+                "ON O.Id = OS.OwnerId", CONNECTION_STRING);
+            DataTable table = new DataTable();
+            adapter.Fill(table);
+            dataGridView1.DataSource = table;
+            connection.Close();
+        }
+    }
+}
